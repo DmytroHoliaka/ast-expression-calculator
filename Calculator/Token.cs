b2f5f22af604f77ca67a9cd401030aab825d2182
HoliakaDmytro <holidim39@gmail.com>
2023-09-22 16:25:45 +0300

    Token: Added class that represent expression token

---
+++ Calculator/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Token
+    {
+        public TokenType Type { get; init; }
+        public double? Value { get; init; }
+
+        public Token(TokenType type, double? value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}

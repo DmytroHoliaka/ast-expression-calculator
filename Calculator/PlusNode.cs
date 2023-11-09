c07dff6aab01c13f79810d4c75f0862955344390
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:48:42 +0200

    PlusNode: Adding a class that represents an addition node

---
+++ Calculator/PlusNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class PlusNode : IAbstractSyntaxTree
+    {
+        public IAbstractSyntaxTree LeftNode { get; private set; }
+        public IAbstractSyntaxTree RightNode { get; private set; }
+
+        public PlusNode(IAbstractSyntaxTree leftNode, IAbstractSyntaxTree rightNode)
+        {
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+
+        public double Eval()
+        {
+            return LeftNode.Eval() + RightNode.Eval();
+        }
+    }
+}

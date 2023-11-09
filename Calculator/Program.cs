3dabba7b2a50d4b5b87e565e1d047756368ad6ba
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:52:58 +0200

    Program: Add code to run the program

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -2,9 +2,36 @@
 {
     public class Program
     {
+        // ToDo: Change "private set" properties to "init"
         public static void Main(string[] args)
         {
-            Console.WriteLine("Form Program.Main");
+            try
+            {
+                //string expression = "1+2*3-(4/5*6+7)-8*9-";
+                string expression = "1-*/*2";
+
+                LexicalAnalysis lexer = new(expression);
+                List<Token> tokens = lexer.GetTokens();
+
+                Parser parser = new(tokens);
+                Console.WriteLine(parser.GetValue());
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

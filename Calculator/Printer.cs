f816a552dcf40c3144e2cffb52fd6fd2a7a8c77d
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:50:50 +0200

    Printer: Add a class for outputting information

---
+++ Calculator/Printer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Printer
+    {
+        public static void Print(List<Token>? tokens)
+        {
+            Console.WriteLine("Tokens: ");
+
+            if (tokens is null)
+            {
+                Console.WriteLine("[null]");
+                return;
+            }
+
+            int count = tokens.Count;
+            Console.Write("[");
+
+            for (int i = 0; i < count; ++i)
+            {
+                Console.Write(tokens[i]);
+
+                if (i < count - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.WriteLine("]");
+        }
+    }
+}

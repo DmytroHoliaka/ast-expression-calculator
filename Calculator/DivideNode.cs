ee50b7f2d9fcb08d31ffbdd7dfb687f3177489ac
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:49:33 +0200

    DivideNode: Add a class that represents a division node

---
+++ Calculator/DivideNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class DivideNode : IAbstractSyntaxTree
+    {
+        public IAbstractSyntaxTree LeftNode { get; private set; }
+        public IAbstractSyntaxTree RightNode { get; private set; }
+
+        public DivideNode(IAbstractSyntaxTree leftNode, IAbstractSyntaxTree rightNode)
+        {
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+
+        public double Eval()
+        {
+            // ToDo: Add check dividing to 0
+            return LeftNode.Eval() / RightNode.Eval();
+        }
+    }
+}

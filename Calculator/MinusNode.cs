3c9f26d2c04870cfc3f6327874ab50b46f4ae0ec
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:49:00 +0200

    MinusNode: Add a class that represents a subtraction node

---
+++ Calculator/MinusNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MinusNode : IAbstractSyntaxTree
+    {
+        public IAbstractSyntaxTree LeftNode { get; private set; }
+        public IAbstractSyntaxTree RightNode { get; private set; }
+
+        public MinusNode(IAbstractSyntaxTree leftNode, IAbstractSyntaxTree rightNode)
+        {
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+
+        public double Eval()
+        {
+            return LeftNode.Eval() - RightNode.Eval();
+        }
+    }
+}

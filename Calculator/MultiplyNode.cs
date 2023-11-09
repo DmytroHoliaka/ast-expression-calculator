31201dee6ed62d46c8b8b0314c027b1e15e1d359
HoliakaDmytro <holidim39@gmail.com>
2023-11-09 12:49:18 +0200

    MultiplyNode: Add a class that represents a multiplication node

---
+++ Calculator/MultiplyNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MultiplyNode : IAbstractSyntaxTree
+    {
+        public IAbstractSyntaxTree LeftNode { get; private set; }
+        public IAbstractSyntaxTree RightNode { get; private set; }
+
+        public MultiplyNode(IAbstractSyntaxTree leftNode, IAbstractSyntaxTree rightNode)
+        {
+            LeftNode = leftNode;
+            RightNode = rightNode;
+        }
+
+        public double Eval()
+        {
+            return LeftNode.Eval() * RightNode.Eval();
+        }
+    }
+}

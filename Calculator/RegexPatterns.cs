d48ab4b9845c1b1e1b890632f3589d89843f1264
HoliakaDmytro <holidim39@gmail.com>
2023-11-11 12:38:04 +0200

    RegexPatterns: Add a class with patterns for recognizing input arguments

---
+++ Calculator/RegexPatterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class RegexPatterns
+    {
+        public const string ExpressionPattern = @"^([ +\-(]*[ ]*\d*[ ]*\.?[ ]*\d*[+\-*/)]*)+$";
+        public const string FilePathPattern = @"^(.*?[\/]*?)([a-zA-Z0-9_\- ]+.txt)$";
+        public const string TextRecordPattern = $"{ExpressionPattern}|{FilePathPattern}";
+        
+        public const string UploadPattern = @"^(-u)$|^(-upload)$";
+        public const string EvalPattern = @"^(-e)$|^(-eval)$";
+    }
+}

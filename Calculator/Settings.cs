9fbc25ce87ea84f4ab9565784d0021270aea742a
HoliakaDmytro <holidim39@gmail.com>
2023-11-11 12:39:21 +0200

    Settings: Add a class for configuring the solution launch

---
+++ Calculator/Settings.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Settings
+    {
+        public string? Record = null;
+        public string? OutputFilePath = null;
+
+        public bool IsUploaded = false;
+        public bool IsEvaled = false;
+
+        public Settings(params string[]? args)
+        {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args), "Application arguments cannot be null");
+            }
+
+            ContentAnalyzer.ParseArgumentsToSettings(args, this);
+
+            if (IsUploaded == true)
+            {
+                string outputFileName = $"output({DateTime.Now.ToString("HH-mm-ss")}).txt";
+
+                Match match = Regex.Match(Record!, RegexPatterns.FilePathPattern);
+                string foldersPath = match.Groups[1].Value;
+
+                OutputFilePath = foldersPath + outputFileName;
+            }
+        }
+    }
+}
